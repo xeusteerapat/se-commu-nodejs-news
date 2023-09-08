3c6924b9bff2ae82f890ca838977e1448ee46c15
xeusteerapat <xeus085@gmail.com>
2023-09-09 00:11:00 +0700

    :tada: [Init]

---
+++ README.md
@@ -0,0 +1,6 @@
+# What's new in Node.js v20 (also from Node.js 16 onwards)
+
+- Stable test runner
+- Load `.env` file
+- Stable `fetch` API
+- `watch` mode
---
+++ index.test.js
@@ -0,0 +1,8 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert/strict';
+
+describe(() => {
+  it('should', () => {
+    assert.strictEqual(1, 1);
+  });
+});
