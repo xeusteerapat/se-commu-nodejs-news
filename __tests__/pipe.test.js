398e802c54f35e9e28069e38a0f336ac50a3dc3c
teerapat-prommarak <teerapat.prommarak@krungthai-axa.co.th>
2023-09-15 10:53:29 +0700

    📦 fix wrong import folder

--- __tests__/pipe.test.js
+++ __tests__/pipe.test.js
@@ -1,6 +1,6 @@
 import { describe, it } from 'node:test';
 import assert from 'node:assert/strict';
-import { pipe } from '../kata.js';
+import { pipe } from '../playground/kata.js';
 
 describe('pipe', () => {
   it('should return the input value when no functions are provided', () => {
@@ -9,17 +9,17 @@ describe('pipe', () => {
   });
 
   it('should apply functions in the correct order', () => {
-    const addTwo = x => x + 2;
-    const multiplyByThree = x => x * 3;
-    const subtractFive = x => x - 5;
+    const addTwo = (x) => x + 2;
+    const multiplyByThree = (x) => x * 3;
+    const subtractFive = (x) => x - 5;
 
     const combinedFunction = pipe(addTwo, multiplyByThree, subtractFive);
     assert.strictEqual(combinedFunction(10), 31); // ((10 + 2) * 3) - 5 = 21
   });
 
   it('should work with functions returning different data types', () => {
-    const addTwoToString = x => (x + 2).toString();
-    const divideByTwo = x => x / 2;
+    const addTwoToString = (x) => (x + 2).toString();
+    const divideByTwo = (x) => x / 2;
 
     const combinedFunction = pipe(addTwoToString, divideByTwo);
     assert.strictEqual(combinedFunction(10), 6); // ((10 + 2).toString() / 2) = '6'
